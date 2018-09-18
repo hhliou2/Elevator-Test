d69143acf841eec0680a66d97c7209af31d0614b
HASAN\Hasan <liouhasan@gmail.com>
2018-09-18 14:08:31 -0700

    Doors Added, Increased speed

---
+++ Elevator Test/Assets/Scripts/DoorMotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMotion : MonoBehaviour {
+
+	public GameObject leftDoor;
+	public GameObject rightDoor;
+
+	public void OpenDoors(float speed) {
+		leftDoor.transform.Translate (new Vector3 (speed, 0));
+		rightDoor.transform.Translate (new Vector3 (-speed, 0));
+	}
+
+	public void CloseDoors(float speed) {
+		leftDoor.transform.Translate (new Vector3 (-speed, 0));
+		rightDoor.transform.Translate (new Vector3 (speed, 0));
+	}
+}
--- Elevator Test/Assets/Scripts/ElevatorMotion.cs
+++ Elevator Test/Assets/Scripts/ElevatorMotion.cs
@@ -6,20 +6,33 @@ public class ElevatorMotion : MonoBehaviour {
 
 	float bottomHeight = 0;
 	float topHeight = 5;
-	float speed = 0.01f;
+	float speed = 0.05f;
 
 	// Use this for initialization
-	void Start () {
-		StartCoroutine (waiter ());
+		void Start () {
+		GameObject doors = GameObject.Find ("Doors");
+		DoorMotion doorMotion = doors.GetComponent<DoorMotion>();
+		StartCoroutine (waiter (doors, doorMotion));
 	}
 
-	IEnumerator waiter() {
+	IEnumerator waiter(GameObject doors, DoorMotion doorMotion) {
+
 		while (this.transform.position.y < topHeight) {
 			this.transform.Translate (new Vector3(0, speed));
 			yield return new WaitForFixedUpdate ();
 		} 
 
-		yield return new WaitForSeconds (3);
+		while (doors.transform.GetChild (0).position.x < 0.745) {
+			doorMotion.OpenDoors(speed);
+			yield return new WaitForFixedUpdate ();
+		}
+
+		yield return new WaitForSeconds (2);
+
+		while (doors.transform.GetChild (0).position.x > 0.245) {
+			doorMotion.CloseDoors(speed);
+			yield return new WaitForFixedUpdate ();
+		}
 
 		while (this.transform.position.y > bottomHeight) {
 			this.transform.Translate (new Vector3(0, -speed));
