93b1f67682950f056938b89391ac82fc8dee9dbb
HASAN\Hasan <liouhasan@gmail.com>
2018-09-18 13:15:10 -0700

    Uploaded Initial Test of a Moving Elevator

---
+++ Elevator Test/Assets/Scripts/ElevatorMotion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorMotion : MonoBehaviour {
+
+	float bottomHeight = 0;
+	float topHeight = 5;
+	float speed = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (waiter ());
+	}
+
+	IEnumerator waiter() {
+		while (this.transform.position.y < topHeight) {
+			this.transform.Translate (new Vector3(0, speed));
+			yield return new WaitForFixedUpdate ();
+		} 
+
+		yield return new WaitForSeconds (3);
+
+		while (this.transform.position.y > bottomHeight) {
+			this.transform.Translate (new Vector3(0, -speed));
+			yield return new WaitForFixedUpdate ();
+		}
+	}
+}
