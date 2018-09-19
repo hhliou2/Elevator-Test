b840df13d3ba31d55b037ae4abbbcd6152efed1f
HASAN\Hasan <liouhasan@gmail.com>
2018-09-19 15:11:33 -0700

    Elevators are now synced over the network, added Menu Scene to begin

--- Elevator Test/Assets/Scripts/ElevatorMotion.cs
+++ Elevator Test/Assets/Scripts/ElevatorMotion.cs
@@ -2,41 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ElevatorMotion : MonoBehaviour {
+public class ElevatorMotion : Bolt.EntityBehaviour<IElevatorState> {
 
 	float bottomHeight = 0;
 	float topHeight = 5;
 	float speed = 0.05f;
 
 	// Use this for initialization
-		void Start () {
+	public override void Attached () {
 		GameObject doors = GameObject.Find ("Doors");
 		DoorMotion doorMotion = doors.GetComponent<DoorMotion>();
+
+		state.SetTransforms (state.ElevatorTransform, transform);
 		StartCoroutine (waiter (doors, doorMotion));
 	}
 
 	IEnumerator waiter(GameObject doors, DoorMotion doorMotion) {
+		while (true) {
+			while (this.transform.position.y < topHeight) {
+				this.transform.Translate (new Vector3(0, speed));
+				yield return new WaitForFixedUpdate ();
+			} 
 
-		while (this.transform.position.y < topHeight) {
-			this.transform.Translate (new Vector3(0, speed));
-			yield return new WaitForFixedUpdate ();
-		} 
+			while (doors.transform.GetChild (0).position.x < 0.745) {
+				doorMotion.OpenDoors(speed);
+				yield return new WaitForFixedUpdate ();
+			}
 
-		while (doors.transform.GetChild (0).position.x < 0.745) {
-			doorMotion.OpenDoors(speed);
-			yield return new WaitForFixedUpdate ();
-		}
+			yield return new WaitForSeconds (2);
 
-		yield return new WaitForSeconds (2);
+			while (doors.transform.GetChild (0).position.x > 0.245) {
+				doorMotion.CloseDoors(speed);
+				yield return new WaitForFixedUpdate ();
+			}
 
-		while (doors.transform.GetChild (0).position.x > 0.245) {
-			doorMotion.CloseDoors(speed);
-			yield return new WaitForFixedUpdate ();
-		}
-
-		while (this.transform.position.y > bottomHeight) {
-			this.transform.Translate (new Vector3(0, -speed));
-			yield return new WaitForFixedUpdate ();
+			while (this.transform.position.y > bottomHeight) {
+				this.transform.Translate (new Vector3 (0, -speed));
+				yield return new WaitForFixedUpdate ();
+			}
 		}
 	}
 }
