611787a6a8baaaa6903eb95ceb92e32ff2510d31
HASAN\Hasan <liouhasan@gmail.com>
2018-09-19 12:34:19 -0700

    Commiting failed changes to revert to original state

---
+++ Elevator Test/Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : Bolt.GlobalEventListener {
+	void OnGUI() {
+		GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
+
+		if (GUILayout.Button("Start Server", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true))) {
+			// START SERVER
+			BoltLauncher.StartServer(UdpKit.UdpEndPoint.Parse("127.0.0.1:27000"));
+		}
+
+		if (GUILayout.Button("Start Client", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true))) {
+			// START CLIENT
+			BoltLauncher.StartClient();
+		}
+
+		GUILayout.EndArea();
+	}
+
+	public override void BoltStartDone() {
+		if(BoltNetwork.isServer)
+			BoltNetwork.LoadScene("Elevator");
+		else  BoltNetwork.Connect(UdpKit.UdpEndPoint.Parse("127.0.0.1:27000"));
+
+	}
+}
---
+++ Elevator Test/Assets/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkCallbacks : Bolt.GlobalEventListener {
+
+	[BoltGlobalBehaviour]
+	public override void SceneLoadLocalDone (string map) {
+		var pos = new Vector3 (0, 0, -5.757f);
+		BoltNetwork.Instantiate (BoltPrefabs.PlayerObject, pos, Quaternion.identity);
+	}
+
+}
